f2ba0735a78abec0a0fe33f19c213b3905ab49d6
Vanessa L Medina <Vanessa L Medina@LAPTOP-SVMPFKG2>
2017-09-18 22:37:55 -0500

    Add project files.

---
+++ Prac3Vanessa/Prac3Vanessa/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Prac3Vanessa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("DATOS INGRESADOS", Nombre.Text + " " + Apellidos.Text + "\n " + Direccion.Text + " \n " + Telefono.Text + " \n" + Carrera.Text + " \n" + Semestre.Text + "\n " + Matricula.Text + "\n " + Correo.Text + "\n " + Github.Text, "OK");
+        }
+
+    }
+}
